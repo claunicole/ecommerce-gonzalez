283f449ae36d116ec32b76cd9d076ba2bd6522d4
claunicole <claunicole.gf@gmail.com>
2022-04-27 09:43:40 -0400

    Actualizacion de NavBar

---
+++ src/components/NavBar.js
@@ -0,0 +1,20 @@
+import './NavBar.css';
+import logo from '../assets/logo.png';
+
+const NavBar = () => {
+return (
+    <header className="header">
+        <img src={logo} className='logo' alt="logo" />
+        <nav>
+            <ul className='nav-container'>
+                <li><a href="#">Inicio</a></li>
+                <li><a href="#">Productos</a></li>
+                <li><a href="#">Ofertas</a></li>
+                <li><a href="#">Contacto</a></li>
+             </ul>
+        </nav>
+    </header>
+)
+}
+
+export default NavBar;
