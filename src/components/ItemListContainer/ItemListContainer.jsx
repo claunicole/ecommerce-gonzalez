0ba417616cd0408fa2011da2705780aa66002db1
claunicole <claunicole.gf@gmail.com>
2022-05-11 11:55:04 -0400

    Mejora del Item List

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -2,6 +2,7 @@ import { toHaveDescription } from "@testing-library/jest-dom/dist/matchers";
 import { useEffect, useState } from "react";
 import ItemList from "../ItemList/ItemList";
 import { productos } from "../../data/data";
+import { Ring } from '@uiball/loaders'
 
   const items = new Promise((resolve) => {
     setTimeout(() => {
@@ -26,7 +27,14 @@ import { productos } from "../../data/data";
     return (
         <div>
             <h1>{greeting}</h1> 
-            {loading ? (<h2>Cargando...</h2>) : (<ItemList productos={productos}/>)}
+            {loading ? (<h2 className="loading">
+              <Ring 
+                size={40}
+                lineWeight={5}
+                speed={2} 
+                color="black" 
+              /></h2>) 
+              : (<ItemList productos={productos}/>)}
         </div>
     )
 }
--- src/data/data.jsx
+++ src/data/data.jsx
@@ -4,6 +4,8 @@ import img3 from "../components/assets/yondu_shirt.JPG"
 import img4 from "../components/assets/boontaEve_shirt.jpg"
 import img5 from "../components/assets/rey_shirt.JPG"
 import img6 from "../components/assets/strange_shirt.jpg"
+import img7 from "../components/assets/trixie_shirt.jpg"
+import img8 from "../components/assets/kylo_shirt.jpg"
 
 export const productos = [
     { id: "1", name: "Polera Black Panther", description: "Polera Unisex Funko", price: "$14", pictureUrl:img1, stock:6},
@@ -11,5 +13,7 @@ export const productos = [
     { id: "3", name: "Polera Yondu", description: "Polera Unisex Funko", price: "$14", pictureUrl:img3, stock:4},
     { id: "4", name: "Polera Boonta Eve", description: "Polera Unisex Funko", price: "$14", pictureUrl:img4, stock:3 },
     { id: "5", name: "Polera Star Wars", description: "Polera Unisex Funko", price: "$14", pictureUrl:img5, stock:3 },
-    { id: "6", name: "Polera Doctor Strange", description: "Polera Unisex Funko", price: "$14", pictureUrl:img6, stock:3 }
+    { id: "6", name: "Polera Doctor Strange", description: "Polera Unisex Funko", price: "$14", pictureUrl:img6, stock:3 },
+    { id: "7", name: "Polera Villanos Star Wars", description: "Polera Unisex Funko", price: "$14", pictureUrl:img7, stock:3 },
+    { id: "8", name: "Polera Kylo Ren", description: "Polera Unisex Funko", price: "$14", pictureUrl:img8, stock:3 }
   ];
